5aa6e147eb9d099f59e7a741adf172c0aba6b6db
vamshi <vamshinani9441@gmail.com>
2023-11-22 16:09:48 +0000

    fifth commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+// import {Switch, Route, Redirect} from 'react-router-dom'
+import {
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom/cjs/react-router-dom.min'
+
+import Home from './components/Home'
+import Login from './components/Login'
+import Jobs from './components/Jobs'
+import NotFound from './components/NotFound'
+import JobItemDetails from './components/JobItemDetails'
+import ProtectedRoute from './components/ProtectedRoute'
+
+import './App.css'
+
+const App = () => (
+  <>
+    <Switch>
+      <Route exact path="/login" component={Login} />
+      <ProtectedRoute exact path="/" component={Home} />
+      <ProtectedRoute exact path="/jobs" component={Jobs} />
+      <ProtectedRoute exact path="/jobs/:id" component={JobItemDetails} />
+      <Route exact path="/not-found" component={NotFound} />
+      <Redirect to="/not-found" />
+    </Switch>
+  </>
+)
+
+export default App
